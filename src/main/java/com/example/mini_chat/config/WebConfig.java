06b148a73348f47bc9df02b0ad460f298daba23e
Gabriel Martins <gabriel.martins@spdata.com.br>
2025-05-04 15:25:38 -0300

    Initial commit
    backend with websocket

--- src/main/java/com/example/mini_chat/config/WebConfig.java
+++ src/main/java/com/example/mini_chat/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*") // Allow requests from localhost:3000
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
